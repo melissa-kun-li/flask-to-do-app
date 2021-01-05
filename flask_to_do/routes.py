46f8f5940e30a85146e0cf44c237239f1771542f
Melissa Li <li.melissa.kun@gmail.com>
2021-01-05 16:31:52 -0500

    Fix merge conflict

--- flask_to_do/routes.py
+++ flask_to_do/routes.py
@@ -21,7 +21,23 @@ def todo():
 @app.route('/update/<task_id>', methods = ['GET', 'POST'])
 # <> pass to value as parameter
 def update(task_id):
-    return 'Updating Task'
+    if request.method == 'POST':
+        old = Task.query.filter_by(id = task_id).first()
+        new_task = request.form['task']
+        if len(new_task) == 0 or len(new_task) > 75:
+            flash('Task should be between 1 and 75 characters')
+            return render_template('update.html', task = old)
+        match = Task.query.filter_by(task = new_task).first()
+        if match == None:
+            old.task = new_task
+            db.session.commit()
+            return redirect(url_for('todo')) 
+        else:
+            flash('Task not updated: task name is already used')
+            return render_template('update.html', task = old)
+    else:
+        task = Task.query.filter_by(id = task_id).first()
+        return render_template('update.html', task = task)
 
 @app.route('/delete/', methods = ['GET', 'POST'])
 def delete():
--- run.py
+++ run.py
@@ -3,7 +3,4 @@ from flask_to_do import app
 
 if __name__ == '__main__':
     db.create_all()
-    # run app in Debug mode so don't have to restart manually after each change to code
-    app.run(debug=True)
-
-# cmd + shift + r to hard reload and empty cache
+    app.run()
