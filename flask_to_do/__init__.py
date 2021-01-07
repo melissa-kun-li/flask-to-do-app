bf5e2fb5562cdf2ede5e80e82d2003c3006d1c26
Melissa Li <li.melissa.kun@gmail.com>
2021-01-07 02:26:02 -0500

    Set up mysql cleardb

--- flask_to_do/__init__.py
+++ flask_to_do/__init__.py
@@ -5,7 +5,7 @@ import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'test')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI', 'todo.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
