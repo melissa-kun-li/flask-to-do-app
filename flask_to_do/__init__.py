96e890f3a6e7198e0a092bb981e95e6fc46c7392
Melissa Li <li.melissa.kun@gmail.com>
2021-01-12 15:02:02 -0500

    Change db to Postgres

--- flask_to_do/__init__.py
+++ flask_to_do/__init__.py
@@ -1,14 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-import pymysql
+# import pymysql
+import psycopg2
 import os
 
 # NEED TO CHANGE FROM CLEARDB TO SOMETHING ELSE
-# MYSQL TO POSTGRES
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'test')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
-app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {'pool_recycle': 60} # clearDB keep connection alive
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('POSTGRES_TODO')
+# app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {'pool_recycle': 60} # clearDB keep connection alive
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
